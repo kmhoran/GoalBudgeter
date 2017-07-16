a32e74b91a979d42e6ce804519abb242b21ca62b
Kevin <kevin.michael.horan@gmail.com>
2017-07-15 17:53:24 -0700

    comment out report service

--- Goal/Services/ReportService.cs
+++ Goal/Services/ReportService.cs
@@ -11,38 +11,38 @@ namespace Goal.Services
 {
     public class ReportService
     {
-        public static List<MonthlySpendingByCategoryDomain> GetMonthlySpendingByCategory()
-        {
-            var categories = new List<MonthlySpendingByCategoryDomain>();
-            //var debits = new List<CategoryDomain>();
-            //var collection = new CategoryCollectionDomain { Credits = credits, Debits = debits };
+        //public static List<MonthlySpendingByCategoryDomain> GetMonthlySpendingByCategory()
+        //{
+        //    var categories = new List<MonthlySpendingByCategoryDomain>();
+        //    //var debits = new List<CategoryDomain>();
+        //    //var collection = new CategoryCollectionDomain { Credits = credits, Debits = debits };
 
-            try
-            {
-                DataProvider.ExecuteCmd(GetConnection, "dbo.select_category_byUserId",
-                    inputParamMapper: delegate (SqlParameterCollection paramCollection)
-                    {
-                        paramCollection.AddWithValue("@userId", userId);
-                    },
-                    map: delegate (IDataReader reader, short set)
-                    {
-                        var category = new MonthlySpendingByCategoryDomain();
-                        int startingIndex = 0;
+        //    try
+        //    {
+        //        DataProvider.ExecuteCmd(GetConnection, "dbo.select_category_byUserId",
+        //            inputParamMapper: delegate (SqlParameterCollection paramCollection)
+        //            {
+        //                paramCollection.AddWithValue("@userId", userId);
+        //            },
+        //            map: delegate (IDataReader reader, short set)
+        //            {
+        //                var category = new MonthlySpendingByCategoryDomain();
+        //                int startingIndex = 0;
 
-                        category.CategoryId = reader.GetSafeInt32(startingIndex++);
-                        category.CategoryName = reader.GetSafeString(startingIndex++);
-                        category.Predicted = reader.GetSafeDouble(startingIndex++);
-                        category.Real = reader.GetSafeDouble(startingIndex++);
+        //                category.CategoryId = reader.GetSafeInt32(startingIndex++);
+        //                category.CategoryName = reader.GetSafeString(startingIndex++);
+        //                category.Predicted = reader.GetSafeDouble(startingIndex++);
+        //                category.Real = reader.GetSafeDouble(startingIndex++);
 
-                        categories.Add(category);
-                    });
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+        //                categories.Add(category);
+        //            });
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        throw e;
+        //    }
 
-            return collection;
-        }
+        //    return categories;
+        //}
     }
 }
