10f4e8f6406b9a79451ae19e38d298d8f288eac4
Kevin <kevin.michael.horan@gmail.com>
2017-07-16 09:08:40 -0700

    move project to new folder

--- Goal/Services/ReportService.cs
+++ Goal/Services/ReportService.cs
@@ -1,4 +1,4 @@
-﻿using Sabio.Data;
+﻿//using Sabio.Data;
 using System.Data.SqlClient;
 using System;
 using System.Collections.Generic;
@@ -6,43 +6,52 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using Goal.Models.Domain.Reports;
+using Sabio.Data;
 
 namespace Goal.Services
 {
-    public class ReportService
+    public class ReportService: BaseService
     {
-        //public static List<MonthlySpendingByCategoryDomain> GetMonthlySpendingByCategory()
-        //{
-        //    var categories = new List<MonthlySpendingByCategoryDomain>();
-        //    //var debits = new List<CategoryDomain>();
-        //    //var collection = new CategoryCollectionDomain { Credits = credits, Debits = debits };
+        public static List<MonthlySpendingByCategoryDomain> GetMonthlySpendingByCategory(string userId)
+        {
+            var categories = new List<MonthlySpendingByCategoryDomain>();
 
-        //    try
-        //    {
-        //        DataProvider.ExecuteCmd(GetConnection, "dbo.select_category_byUserId",
-        //            inputParamMapper: delegate (SqlParameterCollection paramCollection)
-        //            {
-        //                paramCollection.AddWithValue("@userId", userId);
-        //            },
-        //            map: delegate (IDataReader reader, short set)
-        //            {
-        //                var category = new MonthlySpendingByCategoryDomain();
-        //                int startingIndex = 0;
+            try
+            {
+                DataProvider.ExecuteCmd(GetConnection, "dbo.Report_GetMonthlySpendingByCategory",
+                    inputParamMapper: delegate (SqlParameterCollection paramCollection)
+                    {
+                        paramCollection.AddWithValue("@userId", userId);
+                    },
+                    map: delegate (IDataReader reader, short set)
+                    {
+                        var category = new MonthlySpendingByCategoryDomain();
+                        string forcastType;
+                        double averageValue = 0;
+                        double fixedValue = 0;
 
-        //                category.CategoryId = reader.GetSafeInt32(startingIndex++);
-        //                category.CategoryName = reader.GetSafeString(startingIndex++);
-        //                category.Predicted = reader.GetSafeDouble(startingIndex++);
-        //                category.Real = reader.GetSafeDouble(startingIndex++);
+                        int startingIndex = 0;
 
-        //                categories.Add(category);
-        //            });
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        throw e;
-        //    }
+                        category.CategoryId = reader.GetSafeInt32(startingIndex++);
+                        category.CategoryName = reader.GetSafeString(startingIndex++);
+                        category.Real = reader.GetSafeDouble(startingIndex++);
+                        // Determine which category forcast to use
+                        forcastType = reader.GetSafeString(startingIndex++);
+                        averageValue = reader.GetSafeDouble(startingIndex++);
+                        fixedValue = reader.GetSafeDouble(startingIndex++);
 
-        //    return categories;
-        //}
+                        category.Predicted = forcastType == "Average" ? averageValue : fixedValue;
+                        
+
+                        categories.Add(category);
+                    });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return categories;
+        }
     }
 }
